40dd36c66baaa6bccf132be5035ed54c109e285c
Dan Pappo <dpappo@gmail.com>
2021-02-10 16:57:44 -0500

    solved word search PP exercise with Brian

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    const verticalJoin = transpose(letters).join("");
+    const horizontalJoin = letters.map(ls => ls.join('')).join();
 
-module.exports = wordSearch
+    if (horizontalJoin.includes(word)) {
+        return true;
+    } else if (verticalJoin.includes(word)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const transpose = function(matrix) {
+    let returnArray = [];
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        let newArray = [];
+        for (let j = 0; j < matrix.length; j++) {
+            newArray.push(matrix[j][i]);
+        }
+        returnArray.push(newArray);
+    }
+
+    return returnArray.map(ls => ls.join(''));
+    ``
+};
+
+module.exports = wordSearch;
